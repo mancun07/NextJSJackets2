25efffe37be1fc27e8eed51f28edf925ed4ec403
Dmitry <mancun07@yandex.ru>
2022-12-11 11:31:58 +0300

    a new commit

--- components/Photos/PhotosList.js
+++ components/Photos/PhotosList.js
@@ -57,7 +57,7 @@ const PhotosList = (props) => {
                 </Backdrop>
             {/* </div> */}
             <div className={classes.modal}>
-                <Image src={imageSrc} alt='band members' width="200" height="200"
+                <Image src={imageSrc} alt={'band members'} width="200" height="200" aria-hidden="true"
                     layout="responsive"
                     priority={true}
                     />
