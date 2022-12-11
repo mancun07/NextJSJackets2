d7d222f02c2204f7b9ce05badf09288d5a964e57
Dmitry <mancun07@yandex.ru>
2022-12-11 11:42:24 +0300

    a new commit

--- components/News/NewsDetails.js
+++ components/News/NewsDetails.js
@@ -56,7 +56,7 @@ let video = null;
 
     return (
         <div className={classes.singleNews}>
-            <Link href={'/news'}><a><button className={classes.btn}>Обратно к списку новостей</button></a></Link>
+            <Link href={'/news'}><a className={classes.btn}>Обратно к списку новостей</a></Link>
             <div>
                 <Image src={props.singleNews.image} alt={props.singleNews.title}
                 width="600px" height="300px" 
