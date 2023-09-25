273ec3fc6d6331bb75933b6f82520f46ef1592b9
Dmitry <mancun07@yandex.ru>
2023-09-25 20:14:37 +0500

    a new photo is added

--- pages/photos/index.js
+++ pages/photos/index.js
@@ -5,15 +5,15 @@ import Head from 'next/head'
 
 
 const bandPhotos = [
-    {id: v4(), title: 'Band Photo1', src:'/photo1.jpg'},
-    {id: v4(), title: 'Band Photo2', src:'/photo2.JPG'},
-    {id: v4(), title: 'Band Photo3', src:'/photo3.JPG'},
+    {id: v4(), title: 'Band Photo1', src:'/about1.jpg'},
+    {id: v4(), title: 'Band Photo2', src:'/about2.JPG'},
+    {id: v4(), title: 'Band Photo3', src:'/about3.JPG'},
     {id: v4(), title: 'Band Photo4', src:'/about4.jpg'},
     {id: v4(), title: 'Band Photo5', src:'/about5.jpg'},
     {id: v4(), title: 'Band Photo6', src:'/about6.jpg'},
-    {id: v4(), title: 'Band Photo7', src:'/concert1.jpg'},
-    {id: v4(), title: 'Band Photo8', src:'/concert2.jpg'},
-    {id: v4(), title: 'Band Photo9', src:'/concert3.jpg'}
+    {id: v4(), title: 'Band Photo7', src:'/photo1.jpg'},
+    {id: v4(), title: 'Band Photo8', src:'/photo2.jpg'},
+    {id: v4(), title: 'Band Photo9', src:'/photo3.jpg'}
   ]
 
 const PhotosPage = () => {
